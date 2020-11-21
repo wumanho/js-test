5e66e0d8cd72eff695c0f67cb988894b9ba48841
wumanho <wumanho@tutanota.com>
2020-11-21 12:23:22 +0800

    update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require('url')
 var port = process.argv[2]
 
 if(!port){
-    console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+    console.log('请指定端口号')
     process.exit(1)
 }
 
@@ -17,38 +17,34 @@ var server = http.createServer(function(request, response){
     var query = parsedUrl.query
     var method = request.method
 
-    /******** 从这里开始看，上面不要看 ************/
-
     console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-    if(path === '/'){
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`
-            <!DOCTYPE html>
-            <head>
-            <link rel="stylesheet" href="/x">    
-            </head>
-            <body>
-            <h1>hello</h1>
-            </body>
-        `)
-        response.end()
-    } else if(path === '/x'){
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`h1{color: red;}`)
-        response.end()
-    } else{
-        response.statusCode = 404
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你访问的页面不存在`)
-        response.end()
+    response.statusCode = 200
+    // 初始化首页
+    const filePath = path === '/' ? '/index.html' : path
+    const index = filePath.lastIndexOf('.')
+    // suffix 是后缀
+    const suffix = filePath.substring(index)
+    const fileTypes = {
+        '.html':'text/html',
+        '.css':'text/css',
+        '.js':'text/javascript',
+        '.png':'image/png',
+        '.jpg':'image/jpeg'
     }
+    response.setHeader('Content-Type',
+        `${fileTypes[suffix] || 'text/html'};charset=utf-8`)
+    let content
+    try{
+        content = fs.readFileSync(`./public${filePath}`)
+    }catch(error){
+        content = '文件不存在'
+        response.statusCode = 404
+    }
+    response.write(content)
+    response.end()
 
-    /******** 代码结束，下面不要看 ************/
 })
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
-
